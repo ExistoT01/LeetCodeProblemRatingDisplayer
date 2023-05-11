e062e17ca0b4256f5736df2f4931d00fde0da52c
ExistoT01 <1x6tot01@gmail.com>
2023-05-11 13:56:08 +0800

    Version 0.2 1. A 添加了和标题元素一致的元素样式 2. U 优化了代码格式

--- displayer.js
+++ displayer.js
@@ -1,51 +1,53 @@
 // ==UserScript==
 // @name         LeetCode problems rating displayer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Display LeetCode problems rating
 // @author       ExistoT01
 // @match        https://leetcode.cn/problems/*
+// @match        https://leetcode.cn/problems/*/description/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=leetcode.cn
 // @grant        none
 // ==/UserScript==
 
-window.addEventListener(
-  "load",
+const dataUrl = "https://zerotrac.github.io/leetcode_problem_rating/data.json";
+
+const titleSelector =
+  "#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div.w-full.px-5.pt-4 > div > div:nth-child(1) > div.flex-1 > div > div > span";
+
+const containerSelector =
+  "#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div.w-full.px-5.pt-4 > div > div:nth-child(1) > div.flex-1 > div > div";
+
+const shwoRating = () => {
+  // Get the problem ID from the URL
+  fetch(dataUrl)
+    .then((res) => res.json())
+    .then((problemRatings) => {
+      const problemID =
+        document.querySelector(titleSelector).childNodes[0].textContent;
+
+      const problem = problemRatings.find((obj) => obj.ID == problemID);
+
+      let problemRating;
+      if (problem === undefined) {
+        console.log("problem not find");
+        problemRating = undefined;
+      } else {
+        problemRating = parseFloat(problem.Rating).toFixed(2);
+      }
+
+      const ratingElement = document.createElement("p");
+      ratingElement.classList = document.querySelector(titleSelector).classList;
+
+      ratingElement.textContent = `「Problem Rating」: ${problemRating}`;
+      document.querySelector(containerSelector).appendChild(ratingElement);
+    })
+    .catch((error) => console.error(error));
+};
+
+window.onload = function () {
   setTimeout(() => {
-    // Your code here...
     console.log("LeetCode Problem Rating Displayer is running...");
-
-    const dataUrl =
-      "https://zerotrac.github.io/leetcode_problem_rating/data.json";
-
-    // Get the problem ID from the URL
-
-    fetch(dataUrl)
-      .then((res) => res.json())
-      .then((problemRatings) => {
-        const problemID = document.querySelector(
-          "#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div.w-full.px-5.pt-4 > div > div:nth-child(1) > div.flex-1 > div > div > span"
-        ).childNodes[0].textContent;
-
-        const problem = problemRatings.find((obj) => obj.ID == problemID);
-
-        let problemRating;
-        if (problem === undefined) {
-            console.log("problem not find");
-            problemRating = undefined;
-        } else {
-            problemRating = problem.Rating;
-        }
-
-        const ratingElement = document.createElement("p");
-
-        ratingElement.textContent = `Problem Rating: ${problemRating}`;
-        document
-          .querySelector(
-            "#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div.w-full.px-5.pt-4 > div > div:nth-child(1) > div.flex-1 > div > div"
-          )
-          .appendChild(ratingElement);
-      })
-      .catch((error) => console.error(error));
-  }, 2000)
-);
+    shwoRating();
+  }, 1500);
+};
